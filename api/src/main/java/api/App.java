60deb8a93b7f8746414840ea9f9dedb65453934d
M Price <zaptross@outlook.com>
2024-03-10 17:14:45 +1000

    feat(api): update imported api builders

--- api/src/main/java/api/App.java
+++ api/src/main/java/api/App.java
@@ -1,10 +1,6 @@
 package api;
 
-import static com.mongodb.client.model.Filters.eq;
-
 import javax.inject.Singleton;
-
-import org.bson.types.ObjectId;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -15,7 +11,6 @@ import api.api.ApiFactory;
 import api.config.DotenvModule;
 import api.database.MongoFactory;
 import api.database.entities.User;
-import api.database.enums.AuthProvider;
 // generated imports
 import api.generated.CollectionFactory;
 import api.generated.LogsIdApiBuilder;
@@ -23,8 +18,7 @@ import api.generated.RootApiBuilder;
 import api.generated.SolutionsApiBuilder;
 import api.generated.SolutionsIdApiBuilder;
 import api.generated.TasksApiBuilder;
-import api.generated.UsersApiBuilder;
-import api.middleware.AuthHandler;
+import api.generated.UsersEmailApiBuilder;
 import api.providers.jobs.JobProviderModule;
 import dagger.Component;
 
@@ -38,10 +32,10 @@ public class App {
             CollectionFactory.class, // generated
             RootApiBuilder.class, // generated
             LogsIdApiBuilder.class, // generated
-            TasksApiBuilder.class, // generated
             SolutionsApiBuilder.class, // generated
             SolutionsIdApiBuilder.class, // generated
-            UsersApiBuilder.class, // generated
+            TasksApiBuilder.class, // generated
+            UsersEmailApiBuilder.class, // generated
     })
 
     public interface AppBuilder {
@@ -53,13 +47,13 @@ public class App {
 
         LogsIdApiBuilder logsIdApiBuilder();
 
-        TasksApiBuilder tasksApiBuilder();
-
         SolutionsApiBuilder solutionsApiBuilder();
 
         SolutionsIdApiBuilder solutionsIdApiBuilder();
 
-        UsersApiBuilder usersApiBuilder();
+        TasksApiBuilder tasksApiBuilder();
+
+        UsersEmailApiBuilder usersEmailApiBuilder();
 
         Javalin javalin();
     }
@@ -77,39 +71,10 @@ public class App {
         var app = api.javalin();
         api.rootApiBuilder().apply();
         api.logsIdApiBuilder().apply();
-        api.tasksApiBuilder().apply();
         api.solutionsApiBuilder().apply();
         api.solutionsIdApiBuilder().apply();
-        api.usersApiBuilder().apply();
+        api.tasksApiBuilder().apply();
+        api.usersEmailApiBuilder().apply();
         app.start(7070);
-
-        try {
-            User user = api.userCollection().find(eq("email", "test@testing.testing")).first();
-
-            if (user == null) {
-                user = new User();
-                user.id = ObjectId.get();
-                user.authProvider = AuthProvider.GITHUB;
-                user.email = "test@testing.testing";
-
-                var res = api.userCollection().insertOne(user);
-                res.wait();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        app.before("/users/<email>", new AuthHandler());
-        app.get("/users/<email>", ctx -> {
-            try {
-                var collection = api.userCollection();
-                User foundUser = collection.find(eq("email", ctx.pathParam("email"))).first();
-                ctx.json(foundUser);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        app.get("/<name>", ctx -> ctx.result("Hello " + ctx.pathParam("name")));
     }
 }

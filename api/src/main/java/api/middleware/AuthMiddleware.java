1bc8cdbd8900e32ee0fa0d66735424a88940f19a
M Price <zaptross@outlook.com>
2024-03-10 17:02:44 +1000

    feat(api): add optional structure for middlewares

--- api/src/main/java/api/middleware/AuthMiddleware.java
+++ api/src/main/java/api/middleware/AuthMiddleware.java
@@ -1,15 +1,26 @@
 package api.middleware;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import org.jetbrains.annotations.NotNull;
 
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
-public class AuthHandler implements Handler {
+@Singleton
+public class AuthMiddleware implements Handler {
+  @Inject
+  public AuthMiddleware() {
+  }
 
   @Override
   public void handle(@NotNull Context ctx) throws Exception {
     var token = ctx.header("Authorization");
+    if (token == null) {
+      token = ctx.cookie("x-identity-token");
+    }
+
     if (token == null) {
       ctx.status(401).result("Unauthorized");
       ctx.skipRemainingHandlers();

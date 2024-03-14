92e6ed7839caad25dbf309dfb1ce99e98c0f2adc
M Price <zaptross@outlook.com>
2024-03-15 00:17:05 +1000

    feat(api): update user to handle oauth details
    Trim python user

--- jobrunner/database/user.py
+++ jobrunner/database/user.py
@@ -1,17 +1,12 @@
 class User:
-    id = ""
-    email = ""
-    auth_provider = ""
+  def __init__(self) -> None:
+    self.id = ""
 
-    def fromDict(d: dict):
-        usr = User()
-        if d is None:
-            return None
-        if "_id" in d:
-          usr.id = d["_id"]
-        if "email" in d:
-          usr.email = d["email"]
-        if "authProvider" in d:
-          usr.auth_provider = d["authProvider"]
+  def fromDict(d: dict):
+      usr = User()
+      if d is None:
+          return None
+      if "_id" in d:
+        usr.id = d["_id"]
 
-        return usr
+      return usr

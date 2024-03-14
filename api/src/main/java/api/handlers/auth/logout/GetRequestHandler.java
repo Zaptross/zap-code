9b1d3eaa189a3227ae03db18df39c5a51546a69c
M Price <zaptross@outlook.com>
2024-03-15 00:01:58 +1000

    feat(api): add auth endpoints, request handler helper for user profile

---
+++ api/src/main/java/api/handlers/auth/logout/GetRequestHandler.java
@@ -0,0 +1,28 @@
+package api.handlers.auth.logout;
+
+import javax.inject.Inject;
+
+import org.pac4j.javalin.LogoutHandler;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+import api.api.RequestHandler;
+
+public class GetRequestHandler implements RequestHandler {
+  public final Handler authMiddleware;
+
+  @Inject
+  public GetRequestHandler(LogoutHandler authMiddleware) {
+    this.authMiddleware = authMiddleware;
+  }
+
+  public Handler[] before() {
+    return new Handler[] { authMiddleware };
+  }
+
+  @Override
+  public void handle(Context ctx) throws Exception {
+    ctx.status(200).redirect("/");
+  }
+}
